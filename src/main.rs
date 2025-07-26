aaa5059abf1455926a7633531e53de14ca6b14ac
hu <laopo_one@outlook.com>
2025-07-27 02:22:47 +0800

    lox rust impl    扫描4.3

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use std::{
+    env,
+    fs::File,
+    io::{Read, Write},
+    os::windows::process,
+    path::Path,
+    sync::atomic::AtomicBool,
+};
+
+static HAD_ERROR: AtomicBool = AtomicBool::new(false);
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: jlox [script]");
+        std::process::exit(64);
+    } else if args.len() == 2 {
+        read_code_file(&args[1]);
+    } else {
+        run_prompt();
+    }
+}
+
+///扫描代码
+/// 扫描代码的入口函数
+
+fn read_code_file(path: &str) {
+    // 这里可以添加代码扫描的逻辑
+    let path = Path::new(path);
+
+    debug_assert!(
+        path.is_file(),
+        "Expected a file, but got a directory or non-existent path: {}",
+        path.display()
+    );
+
+    if !path.exists() || !path.is_file() {
+        // println!("Path does not exist: {}", path.display());
+        eprintln!("Expected a file,path: {}", path.display());
+        return;
+    }
+
+    // 读取文件内容
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error opening file {}: {}", path.display(), e);
+            return;
+        }
+    };
+    let mut content = String::new();
+    let _ = file.read_to_string(&mut content);
+    run(&content);
+
+    if HAD_ERROR.load(std::sync::atomic::Ordering::SeqCst) {
+        // 如果有错误，退出
+        std::process::exit(65);
+    }
+}
+
+fn run_prompt() {
+    println!("Running in interactive mode...");
+    loop {
+        //这个宏不会自动刷新输出缓冲区，因此需要手动刷新
+        print!(">> ");
+        std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+
+        if std::io::stdin().read_line(&mut input).is_err() {
+            break; // 如果读取失败，退出循环
+        }
+        run(&input);
+        // 重置错误状态
+        HAD_ERROR.store(false, std::sync::atomic::Ordering::SeqCst);
+    }
+}
+
+fn run(code: &str) {
+    let mut scanner = Scanner::new(code);
+    let tokens = scanner.scan_tokens();
+
+    for token in &tokens {
+        println!("{:?}", token);
+    }
+}
+
+fn err(line: u32, msg: &str) {
+    // eprintln!("Error: {}", msg);
+    report(line, "", msg);
+}
+
+fn report(line: u32, wher: &str, msg: &str) {
+    eprintln!("[line {}] Error{}: {}", line, wher, msg);
+    // Ordering::SeqCst：
+
+    // 最严格的内存顺序，保证所有线程观察到的操作顺序一致。
+    // 适用于需要全局一致性的场景（如错误标志）。
+    HAD_ERROR.store(true, std::sync::atomic::Ordering::SeqCst);
+}
---
+++ src/token.rs
@@ -0,0 +1,37 @@
+pub enum TokenType {
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF
+}
+
+
+pub struct Token {
+  pub token_type: TokenType,
+  pub lexeme: String,
+  pub literal: String,
+  pub line: u32,
+}
+
+impl Token {
+  pub fn new(token_type: TokenType, lexeme: String, literal: String, line: u32) -> Self {
+    Self { token_type, lexeme, literal, line }
+  }
+  pub fn to_string(&self) -> String {
+    format!("{:?} {} {}", self.token_type, self.lexeme, self.literal)
+  }
+}
